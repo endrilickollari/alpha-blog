687c6a5358065a4b24a82b65cae50ea57d94a36e
Endri Lickollari <lickollariendri@gmail.com>
2024-03-05 20:42:57 +0100

    method to delete article added, action button added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
