72dc2d1d0ebf040dc6f5536a4c989aefdc2005c6
Endri Lickollari <lickollariendri@gmail.com>
2024-03-15 20:38:23 +0100

    add new styles for editing and flash notices

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,8 +29,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    if @article.update(article_params)
-      flash[:notice] = 'Article was updated successfully'
+    if @article.update(article_params )
+      flash[:notice] = 'Article was updated successfully!'
       redirect_to @article
     else
       render :edit
